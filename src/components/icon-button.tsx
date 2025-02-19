e2844992d56a63f130defab4b2b401dcf7f708bd
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-02-19 20:27:19 -0300

    feat: tela invite e funcionamento formulario

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,11 +1,15 @@
-import { ComponentProps, ReactNode } from "react";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
-export function IconButton(props: ButtonProps) {
+export function IconButton({ className, ...props }: ButtonProps) {
   return (
     <button
-      className="p-1.5 h-12 bg-gray-500 cursor-pointer text-blue rounded-md hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+      className={twMerge(
+        "p-1.5 size-9 flex items-center justify-center bg-gray-500 cursor-pointer text-blue rounded-md hover:bg-blue hover:text-gray-900 transition-colors duration-300",
+        className
+      )}
       {...props}
     />
   );
